c5df78ab5a12f4d528e43426a2cdb02100d1a637
aditya303030 <73743156+aditya303030@users.noreply.github.com>
2022-07-10 23:27:35 +0530

    getting data from random api

--- src/App.js
+++ src/App.js
@@ -1,53 +1,36 @@
 import React,{useEffect,useState} from 'react';
-import axios from 'axios';
 import './App.css';
 
 //https://randomuser.me/api
 
-const fetchRandonData = () => {
-  return axios.get('https://randomuser.me/api')
-    .then(({data}) => {
-      // handle success
-      console.log(data);
-      return data; 
-    })
-    .catch((err) => {
-      // handle error
-      console.log(err);
-    })
-}
 
-const getUserInfo = (userInfo) => {
-  // return userInfo.
-  // const userName =
-  const {name:{first,last}} = userInfo;
-  return `${first} ${last}` 
-}
 
 function App() {
+  const [userdata,setuserData] = useState([]);
+  
+  const fetchData = () => {
+    fetch('https://api.randomuser.me/')
+      .then(response => response.json())
+      .then(data => setuserData(data.results))
+      .catch(error=>console.log(error))
+  }
+  // const getUserInfo = () =>{
 
-  const [data,setdata] = useState('')
-  const [userInfos,setUserInfos] = useState([])
-
-  useEffect(()=> {
-    fetchRandonData().then((randomdata)=> {
-      setdata(JSON.stringify(randomdata,null,2))
-      setUserInfos(randomdata.results)
-    })
+  // }
+  console.log(userdata)
+  useEffect(()=>{
+    fetchData()
   },[])
-  console.log(userInfos)
+
   return (
     <>
-      <div className="App">
-        <pre>
-          {data}
-        </pre>
-        {/* {
-          userInfos.map((userInfo,id)=> {
-            return <pre key={id}>{getUserInfo(userInfo)}</pre>
-          })
-        } */}
-      </div>
+    <div>
+      {/* {
+        data.map((entry,idx)=>{
+          return <pre key={idx}>{entry}</pre>
+        })
+      } */}
+    </div>
     </>
   );
 }
