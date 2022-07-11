9e1adbcf7b8f717a12180b2207d31da37df7e7dc
aditya303030 <73743156+aditya303030@users.noreply.github.com>
2022-07-12 02:04:44 +0530

    api data received, displayed name and picture

--- src/App.js
+++ src/App.js
@@ -2,45 +2,44 @@ import React,{useEffect,useState} from 'react';
 import axios from 'axios';
 import './App.css';
 
-//https://randomuser.me/api
-
-
+//api link
+// https://hub.dummyapis.com/employee?noofrecords=10&idStarts=1001
 
 function App() {
-  const [userdata,setuserData] = useState([]);
-  const [userpicture,setuserpicture] = useState();
-  
+
+  //states
+  const [randomJSONdata,setrandomJSONdata] = useState([])
+
   const fetchData = () => {
-    axios.get('https://api.randomuser.me/')
-      .then((response) => {
-        setuserData(response.data.results[0].name)
-        setuserpicture(response.data.results[0].picture.large)
+    const url = 'https://hub.dummyapis.com/employee?noofrecords=10&idStarts=1001'
+    axios.get(url)
+      .then((res) => {
+        setrandomJSONdata(res.data)
+        console.log(res.data)
       })
-      .then(data => data)
-      .catch(error => console.error(error))
-  }
-  
-  // useEffect(()=>{
-  //   fetchData()
-  // },[])
-  
-  if (userdata) {
-    return (
-      <>
-        <button onClick={fetchData}>Next user data</button>
-        <p>{userdata.title}</p>
-        <p>{userdata.first}</p>
-        <p>{userdata.last}</p>
-        <img src={userpicture} />
-      </>
-    )
+      .catch((err)=>{
+        console.log(err)
+      })
   }
 
-  return (
+  useEffect(() => {
+    fetchData()
+  },[])
+
+  return(
     <>
-      <p>No user data</p>
+      <div>
+        {randomJSONdata.map((datax,idx) => {
+          return <p key={idx}>{datax.firstName}</p>
+          // <img src={datax.imageurl} />
+        })}
+        {randomJSONdata.map((datax,idx) => {
+          // return <p key={idx}>{datax.firstName}</p>
+          return <img key={idx} src={datax.imageUrl} alt="no pic found"/>
+        })}
+      </div>
     </>
-  );
+  )
 }
 
 export default App;
