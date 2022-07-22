d4887d5f744cbc44d1f1a4a83182d2fb09bdf8d6
Aditya Raj Sharma <aditya@Adityas-MacBook-Pro.local>
2022-07-22 22:36:38 +0530

    fetching data from random user api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, {  useEffect, useState } from 'react';
 import './App.css';
 
 //api link
@@ -8,67 +8,25 @@ import './App.css';
 function App() {
 
   //states
-  // const [latitude,setLatitude] = useState('')
-  // const [longitude, setLongitude] = useState('')
-  // const [weatherData, setWeatherData] = useState()
+  const [user,setUser] = useState('')
 
-  // const fetchData = () => {
-  //   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=cbba97b5444bbc7799753a7e3e2456b2`
-  //   axios.get(url)
-  //     .then(response => {
-  //       console.log(response.data)
-  //       const data = JSON.stringify(response.data)
-  //       setWeatherData(data)
-  //     })
-  //     .catch(error => {
-  //       console.log(error)
-  //     })
-  // }
-
-  const [name,setName] = useState('');
-
+  const url = "https://randomuser.me/api"
   const fetchData = () => {
-    const url = "https://reqres.in/api/users"
     axios.get(url)
-      .then((response) => {
-        const datax = (response.data)
-        setName(datax)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    .then(res => console.log(res.data.results[0]))
+    .then(res => setUser(JSON.stringify(res.data.results[0])))
+    .catch(err => console.log(err))
   }
-  useEffect(()=> {
+
+  useEffect(() => {
     fetchData()
   },[])
 
-  console.log(name)
+
   return(
     <>
-      {/* {name.map((namex,id)=> {
-        return <pre key={id}>{namex[0].first_name}</pre>
-      })} */}
-      {
-        name.map((namex,idx) => {
-          return <pre key={idx}>{namex[0].first_name}</pre>
-        })
-      }
-      {/* <div className='container'>
-        Longitude:
-        <input onChange={(e) => setLongitude(e.target.value)} />
-        Latitude:
-        <input onChange={(e) => setLatitude(e.target.value)} />
-        <button  onClick ={fetchData} type="submit" >Submit</button>
-        <div className='JSON'>
-          <h1>Raw JSON</h1>
-          {weatherData}
-        </div>
-        <div className='weather-data'>
-          
-          
-        </div>
-      </div> */}
-    </>
+      {user}
+    </> 
   )
 }
 
