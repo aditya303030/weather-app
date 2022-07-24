c30f452297e195914ccc3b0a08a3202f200b2ae5
Aditya Raj Sharma <aditya@Adityas-MacBook-Pro.local>
2022-07-25 01:10:34 +0530

    fetched data from random api using async await

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, {  useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 //api link
@@ -7,25 +7,53 @@ import './App.css';
 
 function App() {
 
-  //states
-  const [user,setUser] = useState('')
+  
 
-  const url = "https://randomuser.me/api"
-  const fetchData = () => {
-    axios.get(url)
-    .then(res => console.log(res.data.results[0]))
-    .then(res => setUser(JSON.stringify(res.data.results[0])))
-    .catch(err => console.log(err))
-  }
+  //states
+  const [userInfo,setUserInfo] = useState([])
+  const [loading, setLoading] = useState(false);
+  
+  // const fetchData = () => {
+  //   return axios.get(url)
+  //   .then(({data}) => {
+  //     // setUserUnfo(response.data.results[0])
+  //     console.log(data.results) 
+  //     return data;
+  //   })
+  //   .catch(error => console.log(error))
+  // } 
+  // console.log(userInfoJSON)
+
+  // useEffect(() => {
+  //   fetchData().then(randomdata => {
+  //     setUserInfoJSON(JSON.stringify(randomdata.results))
+  //     setUserInfo(randomdata.results)
+  //   })
+  // },[])
 
   useEffect(() => {
-    fetchData()
+    const fetchRandomData = async () => {
+      setLoading(true)
+      const url = 'https://jsonplaceholder.typicode.com/users'
+      const response = await axios.get(url)
+      setUserInfo(response.data)
+      setLoading(false)
+    }
+    fetchRandomData()
   },[])
-
+  
+  console.log(userInfo)
 
   return(
     <>
-      {user}
+      {loading ? (
+        <h4>Loading...</h4>) :
+        (userInfo.map((item,idx) =>
+            // Presently we only fetch 
+            // title from the API 
+            <h4 key={idx}>{item.name}</h4>)
+        )
+      }
     </> 
   )
 }
